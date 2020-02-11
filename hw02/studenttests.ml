3ae4193b2b952e17b3de988661448ece43d6f532
njdom24 <dom32400@aol.com>
2020-02-11 17:23:28 -0500

    Added recursive tetration function test

--- hw02/studenttests.ml
+++ hw02/studenttests.ml
@@ -37,17 +37,77 @@ let power_iter n p = [
   ]
 ]
 
+let tetrate_rec a n = [
+  text "main" [
+    Movq, [~$a; ~%R08];
+    Movq, [~$n; ~%R09];
+    Addq, [~$1; ~%R09];
+    Callq, [~$$"tetrate"];
+
+    Retq, []
+  ];
+  (* Computes power given base and exponent, ends with original values back in R08 and R09, with return in Rax *)
+  text "power"
+  [
+    Pushq, [~%R08];
+    Pushq, [~%R09];
+    Cmpq, [~$0; ~%R09];
+    J Eq, [~$$"zeropower"];
+    Movq, [~%R08; ~%Rax]
+  ];
+  text "loop"
+  [
+    Subq, [~$1; ~%R09];
+    Cmpq, [~$0; ~%R09];
+    J Eq, [~$$"exit_rec"];
+    Imulq, [~%R08; ~%Rax];
+    Jmp, [~$$"loop"]
+  ];
+  text "zeropower"
+  [
+    Movq, [~$1; ~%Rax];
+    Jmp, [~$$"exit_rec"]
+  ];
+  text "exit_rec"
+  [
+    Popq, [~%R09];
+    Popq, [~%R08];
+    Retq, []
+  ];
+  (* Tetrate Code Begins *)
+  text "end_tetrate"
+  [
+    Movq, [~$1; ~%Rax];
+    Jmp, [~$$"exit_rec"]
+  ];
+  text "tetrate"
+  [
+    Pushq, [~%R08];
+    Pushq, [~%R09];
+    Subq, [~$1; ~%R09]; (* Need to make the initial call NOT subtract *)
+    Cmpq, [~$0; ~%R09];
+    J Eq, [~$$"end_tetrate"];
+
+    Callq, [~$$"tetrate"];
+    Movq, [~%Rax; ~%R09];
+    Callq, [~$$"power"]; (* The result will now be in Rax *)
+    Jmp, [~$$"exit_rec"]
+  ];
+]
 
 
 let provided_tests : suite = [
   Test ("Student-Provided Big Test for Part III: Score recorded as PartIIITestCase", [
   ]);
 
-  GradedTest ("Power", 10, [
+  GradedTest ("Power_Zero", 10, [
     ("power1", program_test (power_iter 5 0) 1L);
   ]);
-  GradedTest ("Power", 10, [
-    ("power1", program_test (power_iter 5 3) 125L);
+  GradedTest ("Power_Normal", 10, [
+    ("power2", program_test (power_iter 5 3) 125L);
+  ]);
+  GradedTest ("Tetrate_Rec", 10, [
+    ("tetrate1", program_test (tetrate_rec 2 4) 65536L);
   ]);
 
 ]

667e6e73c084a26bc85b8abba3192afaf5390492
njdom24 <dom32400@aol.com>
2020-02-22 15:17:36 -0500

    Initial arg_loc implementation

--- hw03/backend.ml
+++ hw03/backend.ml
@@ -220,7 +220,15 @@ let compile_lbl_block lbl ctxt blk : elem =
 
   [ NOTE: the first six arguments are numbered 0 .. 5 ] *)
 let arg_loc (n : int) : operand =
-  failwith "arg_loc not implemented"
+  match n with
+  | 0 -> Reg Rdi
+  | 1 -> Reg Rsi
+  | 2 -> Reg Rdx
+  | 3 -> Reg Rcx
+  | 4 -> Reg R08
+  | 5 -> Reg R09
+  | _ -> Ind3 (Lit (Int64.of_int (8 * (n-8))), Rbp)
+  (* Puts the rest on the stack *)
 
 (* We suggest that you create a helper function that computes the
   stack layout for a given function declaration.

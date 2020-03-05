f357e3d60cf72f085796a7bfa89ccb63304a7aec
Michael Chunko <chunkomike@gmail.com>
2020-03-05 21:23:07 +0000

    Additional gep tests

---
+++ hw03/custom_llprograms/gep2.ll
@@ -0,0 +1,11 @@
+%struct = type { i64, [5 x i64], i64 }
+
+@gbl = global %struct { i64 1, [5 x i64] [ i64 2, i64 3, i64 4, i64 5, i64 6 ], i64 7 }
+
+define i64 @main(i64 %argc, i8** %arcv) {
+  %1 = getelementptr %struct, %struct* @gbl, i32 0, i32 0
+  store i64 10, i64* %1
+  %2 = load i64, i64* @gbl
+  ret i64 %2
+}
+
--- hw03/studenttests.ml
+++ hw03/studenttests.ml
@@ -8,15 +8,20 @@ open Gradedtests
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
-let icmp_tests = 
-[ "llprograms/icmp1.ll", 1L
-; "llprograms/icmp2.ll", 0L
-; "llprograms/icmp3.ll", 1L ]
+let gep_extra_tests =
+[ "custom_llprograms/gep1.ll", 10L
+; "custom_llprograms/gep2.ll", 10L
+; "custom_llprograms/gep3.ll", 11L ]
 
-let tetrate_tests = 
+let icmp_tests =
+[ "custom_llprograms/icmp1.ll", 1L
+; "custom_llprograms/icmp2.ll", 0L
+; "custom_llprograms/icmp3.ll", 1L ]
+
+let tetrate_tests =
 [ "custom_llprograms/power1.ll", 4L
 ; "custom_llprograms/power2.ll", 1L
-; "custom_llprograms/power3.ll", 81L 
+; "custom_llprograms/power3.ll", 81L
 ; "custom_llprograms/tetrate1.ll", 16L
 ; "custom_llprograms/tetrate2.ll", 1L
 ; "custom_llprograms/tetrate3.ll", 27L
@@ -25,5 +30,6 @@ let tetrate_tests =
 
 let provided_tests : suite = [
   GradedTest ("icmp tests", 0, executed icmp_tests);
+  GradedTest ("gep_extra_tests", 0, executed gep_extra_tests);
   GradedTest ("tetrate_tests", 0, executed tetrate_tests)
 ]

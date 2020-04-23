cfadb2c656ae501d95c124a2af2c2d50f54622a8
MikeChunko <chunkomike@gmail.com>
2020-04-23 19:04:40 -0400

    More tests

--- hw05/studenttests.ml
+++ hw05/studenttests.ml
@@ -246,13 +246,72 @@ exception MyError
 let my_tests = [
   ("typechecked_bop",
   (fun () ->
-      if (Typechecker.typecheck_exp Tctxt.empty (no_loc (Bop(Mul,no_loc (CInt(2L)), no_loc (CInt(2L))))) = TInt) 
+      if (Typechecker.typecheck_exp Tctxt.empty (no_loc (Bop(Mul,no_loc (CInt(2L)), no_loc (CInt(2L))))) = TInt)
       then ()
-      else failwith "should not fail") )                                                                                   
+      else failwith "should not fail") )
 ; ("failed_typecheck_bop",
   fun () -> (try ( ( let x = Typechecker.typecheck_exp Tctxt.empty (no_loc (Bop(Mul,no_loc (CInt(2L)), no_loc (CBool(true))))) in (if(x=TInt) then (raise MyError) else () ) ) )
    with  Typechecker.TypeError(_) -> () | MyError -> failwith "should not have passed"
-      ) ) 
+      ) )
+]
+
+let unit_tests2 = [
+  "type_bop_add",
+  (fun () ->
+  if (Typechecker.typecheck_exp Tctxt.empty (no_loc (Bop(Add, no_loc (CInt(10L)), no_loc (CInt(10L))))) = TInt) then ()
+      else failwith "should not fail")
+; ("no_type_bop_add",
+  (fun () ->
+      try if(Typechecker.typecheck_exp Tctxt.empty (no_loc (Bop(Mul,no_loc (CInt(10L)), no_loc (CStr("NaN"))))) = TInt) then failwith "should not pass" else ()
+      with e -> ()
+ ))
+]
+
+let unit_tests3 = [
+  "subtype_fun_fun",
+   (fun () ->
+       if Typechecker.subtype_ref Tctxt.empty
+         (RFun ([TBool; TInt; TNullRef RString], (RetVal TInt)))
+         (RFun ([TBool; TInt; TNullRef RString], (RetVal TInt)))
+        then ()
+       else failwith "should not fail")
+; ("no_subtype_fun_fun",
+   (fun () ->
+       if Typechecker.subtype_ref Tctxt.empty
+         (RFun ([TBool; TInt; TNullRef RString], (RetVal TInt)))
+         (RFun ([TBool; TInt; TNullRef RString], (RetVoid)))
+        then failwith "should not succeed" else ())
+  )
+]
+
+let str_tc =
+  let tc = Tctxt.empty
+  in let tc1 = Tctxt.add_struct tc "1" [{fieldName = "x1"; ftyp = TInt}; {fieldName = "x2"; ftyp = TInt}]
+  in let tc2 = Tctxt.add_struct tc1 "2" [{fieldName = "x1"; ftyp = TInt}]
+  in let tc3 = Tctxt.add_struct tc2 "3" [{fieldName = "x1"; ftyp = TInt}; {fieldName = "x2"; ftyp = TBool}]
+  in tc3
+let struct_tests = [
+  "subtype_struct1_struct2",
+   (fun () ->
+       if Typechecker.subtype str_tc (TRef(RStruct("1"))) (TRef(RStruct("2"))) then ()
+       else failwith "should not fail")
+; ("no_subtype_struct1_struct3",
+   (fun () ->
+       if Typechecker.subtype str_tc (TRef(RStruct("1"))) (TRef(RStruct("3"))) then
+         failwith "should not succeed" else ())
+  )
+]
+
+let my_tests1 = [
+  "good",
+   (fun () ->
+       if Typechecker.subtype Tctxt.empty (TRef (RArray TInt)) (TRef (RArray TInt)) then ()
+       else failwith "should not fail")
+; ("bad",
+   (fun () ->
+       if Typechecker.subtype Tctxt.empty (TRef (RArray TInt)) (TRef (RArray TBool)) then
+         failwith "should not succeed" else ())
+  )
 ]
 
 let provided_tests : suite = [
@@ -265,4 +324,24 @@ let provided_tests : suite = [
   GradedTest ("Ed's subtyping tests", 4, subtype_tests1);
   GradedTest ("Elliot's OAT trie test", 4, executed_oat_file studenttests1);
   GradedTest("my_unit_test", 0,my_tests);
+  GradedTest("unit_test_cases", 0, unit_tests2);
+  GradedTest("tri_num_test_case", 0, executed_oat_file [("tri_num.oat", "", "21")]);
+  GradedTest("my_tests", 0, unit_tests3);
+  GradedTest("Execution Tests", 0, executed_oat_file [("shapes.oat", "", "12")]);
+  Test("Semiprime Test",
+  (executed_oat_file [("semiprime.oat", "", "Semiprimes: 4 6 9 10 14 15 21 22 25 26 33 34 35 38 39 46 49 51 55 57 58 62 65 69 74 77 82 85 86 87 91 93 94 95 106 111 115 118 119 121 122 123 129 133 134 141 142 143 145 146 155 158 159 161 166 169 177 178 183 185 187 194 201 202 203 205 206 209 213 214 215 217 218 219 221 226 235 237 247 249 253 254 259 262 265 267 274 278 287 289 291 295 298 299 \nOthers: 2 3 5 7 8 11 12 13 16 17 18 19 20 23 24 27 28 29 30 31 32 36 37 40 41 42 43 44 45 47 48 50 52 53 54 56 59 60 61 63 64 66 67 68 70 71 72 73 75 76 78 79 80 81 83 84 88 89 90 92 96 97 98 99 100 101 102 103 104 105 107 108 109 110 112 113 114 116 117 120 124 125 126 127 128 130 131 132 135 136 137 138 139 140 144 147 148 149 150 151 152 153 154 156 157 160 162 163 164 165 167 168 170 171 172 173 174 175 176 179 180 181 182 184 186 188 189 190 191 192 193 195 196 197 198 199 200 204 207 208 210 211 212 216 220 222 223 224 225 227 228 229 230 231 232 233 234 236 238 239 240 241 242 243 244 245 246 248 250 251 252 255 256 257 258 260 261 263 264 266 268 269 270 271 272 273 275 276 277 279 280 281 282 283 284 285 286 288 290 292 293 294 296 297 0")]));
+  Test("Typechecker struct tests", struct_tests);
+  Test("ant test",
+    executed_oat_file[("ant.oat", "", "0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 1 1 0 0 0 0
+0 0 0 0 1 1 1 0 0 0 0
+0 0 0 0 1 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0")]);
+  GradedTest("my_unit_test1", 0, my_tests1);
 ]

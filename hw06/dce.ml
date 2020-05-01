248946241a6a95115b144c4eb195739106055a24
MikeChunko <chunkomike@gmail.com>
2020-04-30 21:18:41 -0400

    Dead code elimination

--- hw06/dce.ml
+++ hw06/dce.ml
@@ -1,16 +1,17 @@
+(* Author: Michael Chunko                                                     *)
+(* Pledge: I pledge my honor that I have abided by the Stevens Honor System.  *)
 (** Dead Code Elimination  *)
 open Ll
 open Datastructures
 
-
 (* expose a top-level analysis operation ------------------------------------ *)
 (* TASK: This function should optimize a block by removing dead instructions
-   - lb: a function from uids to the live-OUT set at the 
+   - lb: a function from uids to the live-OUT set at the
      corresponding program point
    - ab: the alias map flowing IN to each program point in the block
    - b: the current ll block
 
-   Note: 
+   Note:
      Call instructions are never considered to be dead (they might produce
      side effects)
 
@@ -21,10 +22,23 @@ open Datastructures
 
    Hint: Consider using List.filter
  *)
-let dce_block (lb:uid -> Liveness.Fact.t) 
-              (ab:uid -> Alias.fact)
-              (b:Ll.block) : Ll.block =
-  failwith "Dce.dce_block unimplemented"
+let dce_block (lb:uid -> Liveness.Fact.t) (ab:uid -> Alias.fact) (b:Ll.block) : Ll.block =
+  let rec dce_helper (b_old:Ll.block) (b_new:Ll.block) : Ll.block =
+    match b_old.insns with
+    | [] -> {b_new with insns = List.rev b_new.insns} (*{insns = List.rev b_new.insns; term = b_new.term}*)
+    | (u,i)::tl -> (match i with
+      | Call _        -> dce_helper {b_old with insns = tl} {b_new with insns = (u,i)::b_new.insns}
+      | Store(_,_,Id u') ->
+        let liveness_fact = lb u in
+        let alias_fact    = ab u in
+        if Datastructures.UidS.mem u' (lb u) || Datastructures.UidM.mem (Alias.SymPtr.to_string Alias.SymPtr.MayAlias) alias_fact
+        then dce_helper {b_old with insns = tl} {b_new with insns = (u,i)::b_new.insns}
+        else dce_helper {b_old with insns = tl} b_new
+      | _             -> 
+        if Datastructures.UidS.mem u (lb u)
+        then dce_helper {b_old with insns = tl} {b_new with insns = (u,i)::b_new.insns}
+        else dce_helper {b_old with insns = tl} b_new) in
+  dce_helper b {insns = []; term = b.term}
 
 let run (lg:Liveness.Graph.t) (ag:Alias.Graph.t) (cfg:Cfg.t) : Cfg.t =
 
@@ -35,10 +49,9 @@ let run (lg:Liveness.Graph.t) (ag:Alias.Graph.t) (cfg:Cfg.t) : Cfg.t =
     let lb = Liveness.Graph.uid_out lg l in
 
     (* compute aliases at each program point for the block *)
-    let ab = Alias.Graph.uid_in ag l in 
+    let ab = Alias.Graph.uid_in ag l in
 
     (* compute optimized block *)
     let b' = dce_block lb ab b in
     Cfg.add_block l b' cfg
   ) (Cfg.nodes cfg) cfg
-

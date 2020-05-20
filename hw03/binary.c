85767b245bd2753e25776e6301047a19e19faa9e
MikeChunko <chunkomike@gmail.com>
2020-05-20 11:49:15 -0400

    Refactor

--- hw03/binary.c
+++
@@ -1,8 +0,0 @@
-//Robert Schabacker and Jose Amezquita
-//I pledge my honor that I have abided by the Stevens Honor System.
-#include <stdio.h>
-#include <inttypes.h>
-
-void ll_putchar(int64_t c){
-	putchar(c);
-}
--- hw03/custom_llprograms/icmp3.ll
+++
@@ -1,4 +0,0 @@
-define i64 @main(i64 %argc, i8** %arcv) {
-  %1 = icmp slt i64 4, 5
-  ret i64 %1
-}
